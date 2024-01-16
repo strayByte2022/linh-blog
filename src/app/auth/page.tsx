feb0cc62358f35d6d20ebb01123639eb7fd7dbd1
Vương Hồng Lĩnh <linhhandsom@gmail.com>
2024-01-16 17:34:28 +0700

    refactor: arrange folders

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LeftPanel from '../components/auth/LeftPanel'
+
+const LoginPage = () => {
+  return (
+    <div className='flex'>
+        <LeftPanel/>
+    </div>
+  )
+}
+
+export default LoginPage
---
+++ src/app/components/auth/LeftPanel.tsx
@@ -0,0 +1,14 @@
+import { Card, Typography } from 'antd'
+import React from 'react'
+
+const LeftPanel = () => {
+  return (
+    <div>
+        <Card >
+            <Typography>Login</Typography>
+        </Card>
+    </div>
+  )
+}
+
+export default LeftPanel
